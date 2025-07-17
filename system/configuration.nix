94bec6ce544af11a39c123fd77de9b8f2a3f286e
JonNesvold <jnesvold@neomedsys.io>
2025-07-17 22:17:56 +0200

    king

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,36 +1,36 @@
 { config, lib, pkgs, inputs, isLaptop, hasGPU, user, userSpecifiedBrowsers, ... }:
 {
-	imports = [
-		# Hardware and disk configuration
-		"${inputs.self}/system/hardware-configuration.nix"
-		# "${inputs.self}/system/disko-config.nix"
-		
-		# Core system modules
-		"${inputs.self}/modules/environment.nix"
-		"${inputs.self}/modules/system-packages.nix"
-		"${inputs.self}/modules/zsh.nix"
-		"${inputs.self}/modules/nixvim.nix"
-		"${inputs.self}/modules/ssh-config.nix"
-		
-		# Desktop environment
-		"${inputs.self}/modules/login.nix"
-		"${inputs.self}/modules/i3.nix"
-		
-		# Gaming and graphics (conditional)
-		# "${inputs.self}/modules/steam.nix"
-	#] ++ lib.optionals hasGPU [
-	#	"${inputs.self}/modules/nvidia.nix"
-	#] ++ [
-		# VPN
-#		"${inputs.self}/modules/expressvpn.nix"
-	];
+  imports = [
+    # Hardware and disk configuration
+    "${inputs.self}/system/hardware-configuration.nix"
+    # "${inputs.self}/system/disko-config.nix"
+    
+    # Core system modules
+    "${inputs.self}/modules/environment.nix"
+    "${inputs.self}/modules/system-packages.nix"
+    "${inputs.self}/modules/zsh.nix"
+    "${inputs.self}/modules/nixvim.nix"
+    "${inputs.self}/modules/ssh-config.nix"
+    
+    # Desktop environment
+    "${inputs.self}/modules/login.nix"
+    "${inputs.self}/modules/i3.nix"
+    
+    # Gaming and graphics (conditional)
+    "${inputs.self}/modules/steam.nix"
+] ++ lib.optionals hasGPU [
+    "${inputs.self}/modules/nvidia.nix"
+#	] ++ [
+    # VPN
+#   		"${inputs.self}/modules/expressvpn.nix"
+  ];
 
-	# System identification
-	networking.hostName = "perseus";
+  # System identification
+  networking.hostName = "perseus";
 
-	# Enable flakes
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-	# System state version - NEVER change this after initial install
-	system.stateVersion = "25.05";
+  # System state version - NEVER change this after initial install
+  system.stateVersion = "25.05";
 }
