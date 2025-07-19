119cbc6872872e51b98d9b0c3b334ac9f56add5e
JonNesvold <jnesvold@neomedsys.io>
2025-07-19 14:47:53 +0200

    banna

--- modules/i3.nix
+++ modules/i3.nix
@@ -2,26 +2,30 @@
 {
   # This section declaratively manages all your config files
   environment.etc = {
-    "i3/config".source = "${inputs.self}/i3-config/config";
-    "i3status-rust/config.toml".source = "${inputs.self}/i3status-rust-config/config.toml";
-    "polybar/config.ini".source = "${inputs.self}/polybar-config/config.ini";
+    "i3/config".source = "${inputs.self}/configs/i3-config/config";
+    "i3status-rust/config.toml".source = "${inputs.self}/configs/i3status-rust-config/config.toml";
+    "polybar/config.ini".source = "${inputs.self}/configs/polybar-config/config.ini";
     "polybar/launch.sh" = {
-      source = "${inputs.self}/polybar-config/launch.sh";
+      source = "${inputs.self}/configs/polybar-config/launch.sh";
       mode = "0755";
     };
-    "dunst/dunstrc".source = "${inputs.self}/dunst-config/dunstrc";
-    "rofi/config.rasi".source = "${inputs.self}/rofi-config/config.rasi";
+    "dunst/dunstrc".source = "${inputs.self}/configs/dunst-config/dunstrc";
+    "rofi/config.rasi".source = "${inputs.self}/configs/rofi-config/config.rasi";
+    "alacritty/alacritty.yaml".source = "${inputs.self}/configs/alacritty-config/alacritty.yaml";
+
   };
 
   # This script creates symlinks in your home directory
   system.userActivationScripts.i3-configs = ''
-    mkdir -p ~/.config/{i3,i3status-rust,dunst,polybar,rofi}
+    mkdir -p ~/.config/{i3,i3status-rust,dunst,polybar,rofi,alacritty}
     ln -sf /etc/i3/config ~/.config/i3/config
     ln -sf /etc/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
     ln -sf /etc/polybar/config.ini ~/.config/polybar/config.ini
     ln -sf /etc/polybar/launch.sh ~/.config/polybar/launch.sh
     ln -sf /etc/dunst/dunstrc ~/.config/dunst/dunstrc
     ln -sf /etc/rofi/config.rasi ~/.config/rofi/config.rasi
+    ln -sf /etc/alacritty/alacritty.yaml ~/.config/alacritty/alacritty.yaml
+
   '';
 
   # Enable the picom compositor service
--- modules/login.nix
+++ modules/login.nix
@@ -1,20 +1,26 @@
-services.xserver.displayManager.lightdm = {
-  enable = true;
-  greeters.gtk = {
+{ pkgs, inputs, ... }:
+{
+  # General display manager settings
+  services.displayManager.defaultSession = "none+i3";
+
+  # Specific settings for the LightDM GTK Greeter
+  services.xserver.displayManager.lightdm = {
     enable = true;
-    # Use the same Nerd Font as your desktop for consistency
-    font = {
-      name = "MesloLGS NF";
-      size = 11;
+    greeters.gtk = {
+      enable = true;
+      theme = {
+        package = pkgs.arc-theme;
+        name = "Arc-Dark";
+      };
+      iconTheme = {
+        package = pkgs.arc-icon-theme;
+        name = "Arc";
+      };
+      # All custom settings go in extraConfig
+      extraConfig = ''
+        background=${inputs.self}/assets/wallpaper.png
+        font-name=MesloLGS NF 11
+      '';
     };
-    theme = {
-      package = pkgs.arc-theme;
-      name = "Arc-Dark";
-    };
-    iconTheme = {
-      package = pkgs.arc-icon-theme;
-      name = "Arc";
-    };
-    background = "${inputs.self}/assets/wallpaper.png";
   };
-};
+}
