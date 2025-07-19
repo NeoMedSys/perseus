5a849b861488fd0d798839920914f4156688decf
JonNesvold <jnesvold@neomedsys.io>
2025-07-19 15:17:20 +0200

    king

--- modules/i3.nix
+++ modules/i3.nix
@@ -11,21 +11,21 @@
     };
     "dunst/dunstrc".source = "${inputs.self}/configs/dunst-config/dunstrc";
     "rofi/config.rasi".source = "${inputs.self}/configs/rofi-config/config.rasi";
-    "alacritty/alacritty.yaml".source = "${inputs.self}/configs/alacritty-config/alacritty.yaml";
     "picom.conf".source = "${inputs.self}/configs/picom-config/picom.conf";
+    "alacritty/alacritty.toml".source = "${inputs.self}/configs/alacritty-config/alacritty.toml"; # <-- Updated
   };
 
   # This script creates symlinks in your home directory
   system.userActivationScripts.i3-configs = ''
-    mkdir -p ~/.config/{i3,i3status-rust,dunst,polybar,rofi,alacritty,picom} # <-- Add picom
+    mkdir -p ~/.config/{i3,i3status-rust,dunst,polybar,rofi,alacritty,picom}
     ln -sf /etc/i3/config ~/.config/i3/config
     ln -sf /etc/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
     ln -sf /etc/polybar/config.ini ~/.config/polybar/config.ini
     ln -sf /etc/polybar/launch.sh ~/.config/polybar/launch.sh
     ln -sf /etc/dunst/dunstrc ~/.config/dunst/dunstrc
     ln -sf /etc/rofi/config.rasi ~/.config/rofi/config.rasi
-    ln -sf /etc/alacritty/alacritty.yaml ~/.config/alacritty/alacritty.yml # Corrected target name
     ln -sf /etc/picom.conf ~/.config/picom.conf
+    ln -sf /etc/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml # <-- Updated
   '';
 
   # The services.picom block is now REMOVED
