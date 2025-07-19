aabe11668ef5762261502f522a4719eaf895d1f6
JonNesvold <jnesvold@neomedsys.io>
2025-07-19 15:13:35 +0200

    banan

--- modules/i3.nix
+++ modules/i3.nix
@@ -12,28 +12,21 @@
     "dunst/dunstrc".source = "${inputs.self}/configs/dunst-config/dunstrc";
     "rofi/config.rasi".source = "${inputs.self}/configs/rofi-config/config.rasi";
     "alacritty/alacritty.yaml".source = "${inputs.self}/configs/alacritty-config/alacritty.yaml";
+    "picom.conf".source = "${inputs.self}/configs/picom-config/picom.conf";
   };
 
   # This script creates symlinks in your home directory
   system.userActivationScripts.i3-configs = ''
-    mkdir -p ~/.config/{i3,i3status-rust,dunst,polybar,rofi,alacritty}
+    mkdir -p ~/.config/{i3,i3status-rust,dunst,polybar,rofi,alacritty,picom} # <-- Add picom
     ln -sf /etc/i3/config ~/.config/i3/config
     ln -sf /etc/i3status-rust/config.toml ~/.config/i3status-rust/config.toml
     ln -sf /etc/polybar/config.ini ~/.config/polybar/config.ini
     ln -sf /etc/polybar/launch.sh ~/.config/polybar/launch.sh
     ln -sf /etc/dunst/dunstrc ~/.config/dunst/dunstrc
     ln -sf /etc/rofi/config.rasi ~/.config/rofi/config.rasi
-    ln -sf /etc/alacritty/alacritty.yaml ~/.config/alacritty/alacritty.yaml
+    ln -sf /etc/alacritty/alacritty.yaml ~/.config/alacritty/alacritty.yml # Corrected target name
+    ln -sf /etc/picom.conf ~/.config/picom.conf
   '';
 
-  # Enable the picom compositor service
-  services.picom = {
-    enable = true;
-    backend = "glx";
-    fade = true;
-    shadow = true;
-    settings = {
-      "inactive-opacity-override" = false;
-    };
-  };
+  # The services.picom block is now REMOVED
 }
