5666835b2847509bc0afba37b2e746893f7520d9
JonNesvold <jnesvold@neomedsys.io>
2025-07-23 21:55:43 +0200

    gefixed slack

--- pkgs/sandboxed-slack.nix
+++ pkgs/sandboxed-slack.nix
@@ -1,41 +1,24 @@
 { pkgs, ... }:
-
 pkgs.stdenv.mkDerivation {
   name = "sandboxed-slack";
   version = "1.0";
-
   nativeBuildInputs = [ pkgs.makeWrapper ];
-  buildInputs = [ pkgs.bubblewrap pkgs.slack ];
-
   dontUnpack = true;
-
   installPhase = ''
     mkdir -p $out/bin
-
-    makeWrapper ${pkgs.slack}/bin/slack $out/bin/slack \
-      --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bubblewrap ]} \
-      --add-flags "--disable-gpu-sandbox" \
-      --run "mkdir -p $HOME/.local/share/slack-sandboxed" \
-      --run "exec bwrap \
-        --dev-bind / / \
-        --ro-bind /nix/store /nix/store \
-        --proc /proc \
-        --dev /dev \
-        --ro-bind /etc/resolv.conf /etc/resolv.conf \
-        --symlink usr/lib /lib \
-        --symlink usr/lib64 /lib64 \
-        --symlink usr/bin /bin \
-        --symlink usr/sbin /sbin \
-        \
-        --bind $HOME/.local/share/slack-sandboxed $HOME/.config/Slack \
-        \
-        --ro-bind /var/run/dbus /var/run/dbus \
-        --ro-bind $XDG_RUNTIME_DIR/pulse $XDG_RUNTIME_DIR/pulse \
-        --ro-bind $XDG_RUNTIME_DIR/bus $XDG_RUNTIME_DIR/bus \
-        \
-        --ro-bind /tmp/.X11-unix /tmp/.X11-unix \
-        --setenv DISPLAY $DISPLAY \
-        \
-        ${pkgs.slack}/bin/.slack-wrapped --disable-gpu-sandbox \"\$@\""
+    makeWrapper ${pkgs.bubblewrap}/bin/bwrap $out/bin/slack \
+      --run 'mkdir -p "$HOME/.local/share/slack-sandboxed"' \
+      --add-flags "--dev-bind / /" \
+      --add-flags "--ro-bind /nix/store /nix/store" \
+      --add-flags "--proc /proc" \
+      --add-flags "--dev /dev" \
+      --add-flags "--ro-bind /etc/resolv.conf /etc/resolv.conf" \
+      --add-flags "--ro-bind /var/run/dbus /var/run/dbus" \
+      --add-flags "--ro-bind /tmp/.X11-unix /tmp/.X11-unix" \
+      --add-flags "--bind \"\$HOME/.local/share/slack-sandboxed\" \"\$HOME/.config/Slack\"" \
+      --add-flags "--ro-bind \"\$XDG_RUNTIME_DIR/pulse\" \"\$XDG_RUNTIME_DIR/pulse\"" \
+      --add-flags "--ro-bind \"\$XDG_RUNTIME_DIR/bus\" \"\$XDG_RUNTIME_DIR/bus\"" \
+      --add-flags "--setenv DISPLAY \"\$DISPLAY\"" \
+      --add-flags "${pkgs.slack}/bin/slack"
   '';
 }
