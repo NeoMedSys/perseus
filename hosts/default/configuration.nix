00a9a1cc1b11215d1d036c66aea3454c8347a571
JonNesvold <jnesvold@neomedsys.io>
2026-08-18 21:09:37 +0200

    nofix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -5,7 +5,6 @@
     ({ ... }: { _module.args = { inherit userConfig; }; })
     inputs.dms.nixosModules.default
     # ── Always loaded ──
-	"${inputs.self}/modules/hardware/camera.nix"
     "${inputs.self}/modules/system/environment.nix"
     "${inputs.self}/modules/system/system-packages.nix"
     "${inputs.self}/modules/system/greetd.nix"
